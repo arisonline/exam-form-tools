9232b9348781849ad3cf6e1a4c8bc9af7ecd744b
Arisonline <85434273+arisonline@users.noreply.github.com>
2026-05-14 01:41:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -935,7 +935,7 @@ function triggerDownload(
    UNIVERSAL CROPPER
 ========================================= */
 
-function showCropperPopup(
+async function showCropperPopup(
   inputId,
   previewId,
   popupId
@@ -951,6 +951,8 @@ function showCropperPopup(
     !input.files ||
     !input.files[0]
   ) return;
+
+  await loadCropper();
 
   activePreviewId =
     previewId;
@@ -1188,4 +1190,43 @@ async function loadPdfLib() {
   await new Promise(resolve => {
     script.onload = resolve;
   });
+}
+
+
+
+
+
+
+let cropperLoaded = false;
+
+async function loadCropper() {
+
+  if (cropperLoaded)
+    return;
+
+  await new Promise((resolve) => {
+
+    const css =
+      document.createElement("link");
+
+    css.rel = "stylesheet";
+
+    css.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.css";
+
+    document.head.appendChild(css);
+
+    const script =
+      document.createElement("script");
+
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.js";
+
+    script.onload = resolve;
+
+    document.body.appendChild(script);
+
+  });
+
+  cropperLoaded = true;
 }
